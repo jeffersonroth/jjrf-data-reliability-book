0bab7ae548b120147ecfce08f27e7cbb1098c793
jeffersonroth <jeffersonroth@users.noreply.github.com>
2024-03-05 01:02:41 +0000

    deploy: 8f51565a9b1206e4ad92d0391906cf49f5497953

--- main.rs
+++
@@ -1,8 +0,0 @@
-extern crate mdbook;
-extern crate mdbook_catppuccin;
-extern crate mdbook_mermaid;
-extern crate mdbook_toc;
-
-fn main() {
-    println!("Welcome to Data Reliability Engineering book by Jeff Roth!");
-}
